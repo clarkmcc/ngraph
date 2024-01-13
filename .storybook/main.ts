f6f7518d42313507440794b9208902bddf9b6157
Clark McCauley <clarkmccauley@gmail.com>
2024-01-13 09:20:52 -0700

    Fixing storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,16 @@
 import type { StorybookConfig } from '@storybook/react-vite'
+import { withoutVitePlugins } from '@storybook/builder-vite'
 
 const config: StorybookConfig = {
   stories: ['../lib/**/*.mdx', '../lib/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        docs: false,
+      },
+    },
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
   ],
@@ -14,6 +20,14 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: 'tag',
+  },
+  async viteFinal(config) {
+    return {
+      ...config,
+      plugins: await withoutVitePlugins(config.plugins, [
+        'vite:lib-inject-css',
+      ]),
+    }
   },
 }
 export default config
