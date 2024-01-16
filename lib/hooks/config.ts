46dfb0d68d18a39d857455c3267753c035dfc5a7
Clark McCauley <clarkmccauley@gmail.com>
2024-01-15 23:12:20 -0700

    Fixed circular dependency

--- lib/hooks/config.ts
+++ lib/hooks/config.ts
@@ -1,10 +1,11 @@
 import { JSXElementConstructor, useMemo } from 'react'
 import { useGraphConfig } from '../context/GraphConfigContext'
 import { GraphConfig, IGraphConfig } from '../config.ts'
+import { buildNode } from '../node-types.tsx'
 
 export function useNodeTypes(): Record<string, JSXElementConstructor<any>> {
   const [config] = useGraphConfig()
-  return useMemo(() => config.getNodeComponents(), [config])
+  return useMemo(() => config.getNodeComponents(buildNode), [config])
 }
 
 export function useBuildGraphConfig(
