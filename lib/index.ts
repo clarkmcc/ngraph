4e9418401607a88326497a05fe117c7e82de9ee2
Clark McCauley <clarkmccauley@gmail.com>
2024-01-27 12:12:23 -0700

    Exporting new context

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,5 @@
 export { GraphConfig, type InputProps } from './config'
 export { NodeGraphEditor } from './NodeGraphEditor'
-export { GraphConfigProvider } from './context/GraphConfigContext'
 export { useBuildGraphConfig } from './hooks/config.ts'
 export {
   useNodeCollapsed,
@@ -16,5 +15,10 @@ export { NodeSelectField } from './components/NodeSelectField.tsx'
 export { NodeDenseLinkedField } from './components/NodeDenseLinkedField.tsx'
 export { NodeLinkedField } from './components/NodeLinkedField.tsx'
 export { NodeOutputField } from './components/NodeOutputField.tsx'
-export { GraphContext, GraphProvider } from './context/GraphContext'
+export {
+  GraphContext,
+  GraphProvider,
+  useGraphStore,
+  useGraphApi,
+} from './context/GraphContext'
 export { registerLayoutEngine } from './layout/layout.ts'
